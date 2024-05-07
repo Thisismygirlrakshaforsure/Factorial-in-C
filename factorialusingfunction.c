ad55be3a6d9cc551a3d51f15cf343787c9e19aa1
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-05-07 19:19:44 +0545

    Create factorialusingfunction.c

---
+++ factorialusingfunction.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int fact(int,int);
+void main()
+{
+  int n,d;
+printf("enter any number");
+scanf("%d",&n):
+d=fact();
+printf("factorial of %d is %d", n,d);
+getch();
+}
+int fact()
+{
+  int i=1, s=1;
+for(i=1; i<=n; i++)
+{
+s=s*i
+  }
+return(s);
+}
